11da0cee91f540e46eee2c10303417d213e18d85
Anand Singh <anand.singh@searshc.com>
2016-09-10 00:49:30 +0530

    JDBC Configured

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,20 +1,48 @@
 package hello;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		log.info("Creating Tables");
+		jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+		jdbcTemplate.execute("CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+		List<Object[]> splitUpNames = Arrays.asList("Anand Singh", "Nandan Kumar", "Abhinav Mazumdar", "Abhinav Puzikunathu").stream()
+				.map(name -> name.split(" ")).collect(Collectors.toList());
+		splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+		jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES(?,?)", splitUpNames);
+		log.info("Querying for customer records where first_name='Abhinav':");
+		jdbcTemplate.query("SELECT id, first_name, last_name FROM customers WHERE first_name=?",
+						new Object[] { "Abhinav" }, (rs, rowNum) -> new Customer(rs.getLong("id"),
+								rs.getString("first_name"), rs.getString("last_name")))
+				.forEach(customer -> log.info(customer.toString()));
+
 	}
 
 	@Bean
@@ -23,7 +51,7 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
 			Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
 			log.info(quote.toString());
